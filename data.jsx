f05574e154b9ab808eac3d8c6d0a12ebf762f536
ATHARV DALAL <77956535+phythdj24@users.noreply.github.com>
2023-02-15 22:39:38 +0530

    Add files via upload

---
+++ data.jsx
@@ -0,0 +1,48 @@
+const data = {
+  cardData: [
+    {
+        id:1,
+        img:'https://images.unsplash.com/photo-1594636797501-ef436e157819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtaW5nJTIwZGVzayUyMHNldHVwfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        title:'Blog 1',
+        desc:'Descrpation 1'
+
+    },
+    {
+        id:2,
+        img:'https://images.unsplash.com/photo-1594636797501-ef436e157819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtaW5nJTIwZGVzayUyMHNldHVwfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        title:'Blog 2',
+        desc:'Descrpation 2'
+
+    },
+    {
+        id:3,
+        img:'https://images.unsplash.com/photo-1594636797501-ef436e157819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtaW5nJTIwZGVzayUyMHNldHVwfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        title:'Blog 3',
+        desc:'Descrpation 3'
+
+    },
+    {
+        id:4,
+        img:'https://images.unsplash.com/photo-1594636797501-ef436e157819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtaW5nJTIwZGVzayUyMHNldHVwfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        title:'Blog 4',
+        desc:'Descrpation 4'
+
+    },
+    {
+        id:5,
+        img:'https://images.unsplash.com/photo-1594636797501-ef436e157819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtaW5nJTIwZGVzayUyMHNldHVwfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        title:'Blog 5',
+        desc:'Descrpation 5'
+
+    },
+    {
+        id:6,
+        img:'https://images.unsplash.com/photo-1594636797501-ef436e157819?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtaW5nJTIwZGVzayUyMHNldHVwfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+        title:'Blog 6',
+        desc:'Descrpation 6'
+
+    },
+  ]
+}
+
+export default data
